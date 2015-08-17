79d5a19aa487500f919d45f6c4a19731fe29888d
Gregory <2crazymail@gmail.com>
2015-08-18 00:40:20 +0300

    Add home page

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,35 @@ ActiveRecord::Schema.define(version: 20150817201755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "hotels", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "stars"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "room_type"
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reservations", ["room_id"], name: "index_reservations_on_room_id", using: :btree
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer  "number"
+    t.integer  "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "hotel_id"
+  end
+
+  add_index "rooms", ["hotel_id"], name: "index_rooms_on_hotel_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -26,4 +54,7 @@ ActiveRecord::Schema.define(version: 20150817201755) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "rooms", "hotels"
 end
